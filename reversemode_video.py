4e390ff6b1861169a2c245659ebaae146119ffaa
DWAIPAYAN-CODDER <53427936+DWAIPAYAN-codder@users.noreply.github.com>
2019-10-15 09:56:44 +0530

    Create reversemode_video.py

---
+++ reversemode_video.py
@@ -0,0 +1,13 @@
+ frame1 = stream.get(cv2.CAP_PROP_POS_FRAMES)
+    stream.set(cv2.CAP_PROP_POS_FRAMES, frame1 + speed)
+
+    grabbed, frame = stream.read()
+    if not grabbed:
+        exit()
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image = PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+    if flag:
+        canvas.create_text(134, 26, fill="black", font="Times 26 bold", text="Decision Pending")
+    flag = not flag
