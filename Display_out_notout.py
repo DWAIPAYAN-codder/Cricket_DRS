6cb4fafaf7c57ddd7e03f6c0611b3d6e39c4908d
DWAIPAYAN-CODDER <53427936+DWAIPAYAN-codder@users.noreply.github.com>
2019-10-15 10:06:23 +0530

    Display whether a player is out or notout

---
+++ Display_out_notout.py
@@ -0,0 +1,23 @@
+if decision == 'out':
+        decisionImg = "out.png"
+    else:
+        decisionImg = "not_out.png"
+    frame = cv2.cvtColor(cv2.imread(decisionImg), cv2.COLOR_BGR2RGB)
+    frame = imutils.resize(frame, width=SET_WIDTH, height=SET_HEIGHT)
+    frame = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(frame))
+    canvas.image = frame
+    canvas.create_image(0,0, image=frame, anchor=tkinter.NW)
+
+
+def out():
+    thread = threading.Thread(target=pending, args=("out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is out")
+
+
+def not_out():
+    thread = threading.Thread(target=pending, args=("not out",))
+    thread.daemon = 1
+    thread.start()
+    print("Player is not out")
