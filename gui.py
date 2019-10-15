58863e800f456935ca4b4985154169d42f8ffeca
DWAIPAYAN-CODDER <53427936+DWAIPAYAN-codder@users.noreply.github.com>
2019-10-15 10:08:18 +0530

    Create gui using Tkinter and set height and width

---
+++ gui.py
@@ -0,0 +1,11 @@
+SET_WIDTH = 650
+SET_HEIGHT = 368
+
+
+window = tkinter.Tk()
+window.title("CodeWithHarry Third Umpire Decision Review Kit")
+cv_img = cv2.cvtColor(cv2.imread("welcome.png"), cv2.COLOR_BGR2RGB)
+canvas = tkinter.Canvas(window, width=SET_WIDTH, height=SET_HEIGHT)
+photo = PIL.ImageTk.PhotoImage(image=PIL.Image.fromarray(cv_img))
+image_on_canvas = canvas.create_image(0, 0, ancho=tkinter.NW, image=photo)
+canvas.pack()
