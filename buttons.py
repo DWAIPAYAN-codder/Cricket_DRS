52b172befa18f100dc21a4690a1a1bf27ee0fb4f
DWAIPAYAN-CODDER <53427936+DWAIPAYAN-codder@users.noreply.github.com>
2019-10-15 10:10:06 +0530

    Create buttons
    Buttons to control playback

---
+++ buttons.py
@@ -0,0 +1,18 @@
+btn = tkinter.Button(window, text="<< Previous (fast)", width=50, command=partial(play, -25))
+btn.pack()
+
+btn = tkinter.Button(window, text="<< Previous (slow)", width=50, command=partial(play, -2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (slow) >>", width=50, command=partial(play, 2))
+btn.pack()
+
+btn = tkinter.Button(window, text="Next (fast) >>", width=50, command=partial(play, 25))
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Out", width=50, command=out)
+btn.pack()
+
+btn = tkinter.Button(window, text="Give Not Out", width=50, command=not_out)
+btn.pack()
+window.mainloop()
